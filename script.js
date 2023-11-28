e0b0dfb8509bb588ee3d5b5ca2f6ba1195a5a60c
Joshua Owusu <joshbillion1@gmail.com>
2023-11-28 06:04:25 +0000

    Commit auto_update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function cursor() {
   let cursor = document.querySelector(".cursor");
   let body = document.querySelector("body");
 
-  body.addEventListener("mousemove", (e) =>
+  body.addEventListener("mousemove", (e) => {
     gsap.to(cursor, {
       x: e.x + "px",
       y: e.y + "px",
-    })
-  );
+    });
+  });
 }
 cursor();
